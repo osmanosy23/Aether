3b6b60bc341ed1600fac370554dffe89fe028fe1
osmanosy23 <osmanosy23@gmail.com>
2022-11-23 03:24:21 -0500

    Implement QueryPic.kt and connect to NASA API
    - Added imageview (pic of day) and navbar to activity_main.xml
    
    - Implemented QueryPic.kt to utilize NASA's API to obtain NASA's image of the day and display it on home
    
    -  Used glide to display image

---
+++ app/src/main/java/QueryPic.kt
@@ -0,0 +1,13 @@
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class QueryPic(
+    val date : String,
+    val explanation : String,
+    val hdurl : String,
+    val media_type : String,
+    val service_version : String,
+    val title : String,
+    val url: String
+
+)
--- app/src/main/java/com/example/aether/MainActivity.kt
+++ app/src/main/java/com/example/aether/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.aether
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import getPicture
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val picofday : ImageView = findViewById(R.id.myImageView)
+
+        getPicture(this) { myUrlArgument->
+            Glide
+                .with(this)
+                .load(myUrlArgument)
+                .centerCrop()
+                .placeholder(R.drawable.telescope_vec)
+                .into(picofday)
+        }
     }
 }
